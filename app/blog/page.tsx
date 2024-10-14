0b37429244c3c41d52b9157418fe2f89f79d8112
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2024-10-14 13:52:22 -0400

    Mage posts page dynamic

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import prisma from '@/lib/prisma'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Blogs() {
   const posts = await prisma.post.findMany({
     orderBy: {
